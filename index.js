ca82d9d72d297692b67439f16762fdd121145f5e
nsoulier <nathan.soulier@hardis-group.com>
2023-09-27 10:14:27 +0200

    fixed prefix (back to \!) & presence

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 require("log-node")();
 const fs = require('node:fs')
 const path = require('node:path')
-const { Client, IntentsBitField, Collection,  EmbedBuilder, ButtonStyle } = require('discord.js')
+const { Client, IntentsBitField, Collection, EmbedBuilder, ButtonStyle } = require('discord.js')
 const { ActionRowBuilder, ButtonBuilder } = require("@discordjs/builders");
 const { sendPlayingMessage, deleteLastMessage } = require("./utils/utils");
 const { token } = require('./config.json')
-const PREFIX = '&'
+const PREFIX = '!'
 const client = new Client({ intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.DirectMessages, IntentsBitField.Flags.GuildVoiceStates, IntentsBitField.Flags.MessageContent] })
 const log = require('log')
 
@@ -32,10 +32,11 @@ client.player = player
 
 client.once('ready', () => {
 	log.notice('Ready!')
-	
-	client.user.setActivity("blo-blo-blo-blo-blop | !h", {
-		type: "PLAYING",
-	})
+
+	client.user.setPresence({
+		activities: [{ name: `discord.js v14`, type: ActivityType.Watching }],
+		status: 'blo-blo-blo-blo-blop | !h',
+	});
 })
 
 // Registering listeners
@@ -55,14 +56,14 @@ client
 		try {
 			await command.execute(message, args)
 		} catch (error) {
-			const embedError = new  EmbedBuilder()
+			const embedError = new EmbedBuilder()
 				.setColor('#ff0000')
 			if (error instanceof Error) {
 				embedError.setDescription(error.message)
 			} else {
 				embedError.setDescription('Marche po, contactez blop >:(')
 			}
-			message.channel.send({embeds : [embedError]})
+			message.channel.send({ embeds: [embedError] })
 			log.error(error)
 		}
 	})
@@ -72,14 +73,14 @@ client
 		if (!interaction.isButton()) return;
 		let command = client.commands.get(interaction.customId)
 		if (!command) return
-		
+
 		try {
 			await command.execute(interaction, null)
 		} catch (error) {
-			const embedError = new  EmbedBuilder()
-			.setColor('#ff0000')
-			.setDescription('Marche po, contactez blop >:(')
-			interaction.channel.send({embeds : [embedError]})
+			const embedError = new EmbedBuilder()
+				.setColor('#ff0000')
+				.setDescription('Marche po, contactez blop >:(')
+			interaction.channel.send({ embeds: [embedError] })
 
 			log.error(error)
 		}
@@ -110,21 +111,21 @@ client.player.events
 
 client.player.events
 	.on('willAutoPlay', async (queue, tracks, done) => {
-	// Autoplay Next Track Selection Algorithm
-	// Select a random track among tracks which title is not in the history
-	const history = queue.history.tracks.map(t => t.title)
-	let selectedTrack = Util.randomChoice(tracks.slice(0, 10)); 
-	console.log(history, tracks.map(t => t.title))
-	let i = 0
-	while (history.includes(selectedTrack.title)) {
-		selectedTrack = Util.randomChoice(tracks.slice(0, ++i+10)); 
-		if (i+10 > tracks.length) {
-			break // Avoid infinite loop
+		// Autoplay Next Track Selection Algorithm
+		// Select a random track among tracks which title is not in the history
+		const history = queue.history.tracks.map(t => t.title)
+		let selectedTrack = Util.randomChoice(tracks.slice(0, 10));
+		console.log(history, tracks.map(t => t.title))
+		let i = 0
+		while (history.includes(selectedTrack.title)) {
+			selectedTrack = Util.randomChoice(tracks.slice(0, ++i + 10));
+			if (i + 10 > tracks.length) {
+				break // Avoid infinite loop
+			}
 		}
-	}
-	
-	return done(selectedTrack || null);
-})
+
+		return done(selectedTrack || null);
+	})
 
 client.login(token)
 
